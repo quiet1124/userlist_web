aec4f8af359d7ec8b07d8998167c2c6268cf282c
choiyechan <homestayout@gmail.com>
2020-06-17 12:19:14 +0900

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express')
-
+var path = require('path')
 var app = express() 
 
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.get('/hello', (request, response)=>{
+    // console.log(request)
+    response.render('hello.ejs')
+})
+
+// console.log(path.join(__dirname, '/views'))
+
 var port = 8000;
-app.listen(port, function(){
+app.listen(port, ()=>{
     console.log(`Server is running at http://localhost:8080`)
 })
